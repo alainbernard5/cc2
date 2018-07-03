04824581c3a26420eac4eba317e9d00b2bf14bea
Your Name <you@example.com>
2018-07-03 16:53:58 +0000

    Tue Jul  3 16:53:58 UTC 2018

--- test/test.sh
+++ test/test.sh
@@ -3,8 +3,8 @@
 nohup docker run  --rm=true --privileged --cap-add ALL  -v /tmp/share:/tmp/share --interactive alainbernard2/test-c3 &
 
 echo "Launching tests"
-sleep 120
+sleep 300
 echo "Tests are all OK"
 
-tail -n 100  nohup.out
+tail -n 20  nohup.out
 
