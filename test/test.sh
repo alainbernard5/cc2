52867ede6efc0330044e4e9ff2af3130d1fbf8b7
Your Name <you@example.com>
2018-07-05 11:17:41 +0000

    Thu Jul  5 11:17:41 UTC 2018

--- test/test.sh
+++ test/test.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-nohup docker run  --rm=true --privileged --cap-add ALL  -v /tmp/share:/tmp/share --interactive alainbernard2/test-c3 &
+nohup docker run --interactive alainbernard2/test-c3 &
 
 echo "Launching tests"
-sleep 300
+if [ "$APPVEYOR" == "true" ];then
+   sleep 600
+else
+   sleep 300
+fi
 echo "Tests are all OK"
 
 tail -n 20  nohup.out
